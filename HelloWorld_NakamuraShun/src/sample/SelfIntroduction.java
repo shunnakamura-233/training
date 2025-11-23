947eac4d0ea10c2137ad6ca5ca0dd3c7c048d6e5
shunt <shunt@DESKTOP-RMS6MJS>
2025-11-23 23:31:25 +0900

    Java導入　No.2.3

---
+++ HelloWorld_NakamuraShun/src/sample/SelfIntroduction.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "中村駿";
+		int age = 18;
+		String hobby = "旅行";
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
